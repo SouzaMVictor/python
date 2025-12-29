717883754343408daecf42f6fbee1ec46006eebb
Victor Souza <souzamvictor@gmail.com>
2025-12-29 19:10:11 -0300

    minor changes

--- compras.py
+++ compras.py
@@ -1,12 +1,12 @@
 produto = "Arroz"
-preco_unitario = 5.78
+preco_unitario = 5.79
 quantidade = 3
 
 # calculo do valor total
 valor_total = preco_unitario * quantidade
 
 # aplicar desconto de 10% se a quantidade for maior que 2
-if quantidade >2:
+if quantidade > 2:
     # valor_total = valor_total * 0.9
     # depurando o erro - e se eu colocar 0.8 no desconto
     valor_total = valor_total * 0.8
