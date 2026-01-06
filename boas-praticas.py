5e9be0f0dcd224cf37023361f1e2ee8415d6bc8e
Victor Souza <souzamvictor@gmail.com>
2026-01-06 15:22:10 -0300

    separacao de responsabilidades

--- boas-praticas.py
+++ boas-praticas.py
@@ -16,15 +16,26 @@
 
 #geralmente usa-se verbo no infinitivo para nome de funcoes
 
-def calcular_media(notas):
-    media = sum(notas) / len(notas)
-    if media >= 6:
-        print("aprovado")
-    else:
-        print("reprovado")
+# def calcular_media(notas):
+#     media = sum(notas) / len(notas)
+#     if media >= 6:
+#         print("aprovado")
+#     else:
+#         print("reprovado")
 
 #snake_case = funçoes e variaveis 
 #camelCase = nao usado em python
 #PascalCase = recomednado parar classes em python
 #kebab-case = nao usado em python, acha que é subtração
 #SCREAMING_SNAKE_CASE = usado para constantes
+
+#separar conceitos em funcoes diferentes. dividir responsabilidades
+
+def calcular_media(notas):
+    return sum(notas) / len(notas)
+
+def verificar_aprovacao(media):
+    if media >= 6:
+        return "aprovado"
+    else:
+        return "reprovado"
