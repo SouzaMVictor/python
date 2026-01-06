100ba75bd0628395f18ccfb8440ae94762ee9b96
Victor Souza <souzamvictor@gmail.com>
2026-01-06 15:15:38 -0300

    better code. with comments to explain

--- boas-praticas.py
+++ boas-praticas.py
@@ -1,6 +1,15 @@
-def f(n):
-    x = sum(n) / len(n)
-    if x >= 6:
+# def f(n):
+#     x = sum(n) / len(n)
+#     if x >= 6:
+#         print("aprovado")
+#     else:
+#         print("reprovado")
+
+#usar nomes descritivos - facilita leitura de codigo
+
+def calcula_media(notas):
+    media = sum(notas) / len(notas)
+    if media >= 6:
         print("aprovado")
     else:
         print("reprovado")
