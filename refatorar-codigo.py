9ff8f1cb7e5480ba3499402e1278f1ffa0da7586
Victor Souza <souzamvictor@gmail.com>
2026-01-06 15:24:40 -0300

    initial commit for refatorar codigo

---
+++ refatorar-codigo.py
@@ -0,0 +1,17 @@
+nota1 = 7
+nota2 = 6
+media = (nota1 + nota2) / 2
+if media >= 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+nota3 = 8
+nota4 = 9
+media2 = (nota3 - nota4) / 2
+if media2 > 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+#codigo redundante
