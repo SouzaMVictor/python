b0ec805c0613a436494fafcd079390b558c34672
Victor Souza <souzamvictor@gmail.com>
2026-01-06 15:19:06 -0300

    tipos de escritas

--- boas-praticas.py
+++ boas-praticas.py
@@ -7,9 +7,24 @@
 
 #usar nomes descritivos - facilita leitura de codigo
 
-def calcula_media(notas):
+# def calcula_media(notas):
+#     media = sum(notas) / len(notas)
+#     if media >= 6:
+#         print("aprovado")
+#     else:
+#         print("reprovado")
+
+#geralmente usa-se verbo no infinitivo para nome de funcoes
+
+def calcular_media(notas):
     media = sum(notas) / len(notas)
     if media >= 6:
         print("aprovado")
     else:
         print("reprovado")
+
+#snake_case = funçoes e variaveis 
+#camelCase = nao usado em python
+#PascalCase = recomednado parar classes em python
+#kebab-case = nao usado em python, acha que é subtração
+#SCREAMING_SNAKE_CASE = usado para constantes
