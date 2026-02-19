2b57e94c0e4c3aab1857a10214f51510dccad867
Victor Souza <souzamvictor@gmail.com>
2026-02-19 19:16:52 -0300

    estrutura basica

---
+++ estrutura.py
@@ -0,0 +1,25 @@
+# tipos primitivos de dados
+
+# int: numeros inteiro
+# float: numeros decidmais
+# str: cadeia de caracteres
+# bool: valores logicos (True ou False)
+
+# operadores aritmeticos: + - * / // % **
+# operadores logicos: and or not
+# operadores de strings: +, *
+
+# operadores de comparação: ==, !=, >, <, >=, <=
+
+# conversao de tipos
+    # x=5.7
+    # int(x) # converte pra inteiro == 5
+    # str(x) # converte pra string == '5.7'
+    # float(x) # converte pra float == 5.7
+    # booleanos com dados sao True e se sao nulos ou vazios sao considerados False 
+
+# concatenação de strings
+    # 'a' + 'b' == 'ab'
+# multiplicacao de strings
+    # 'a' * 3 == 'aaa'
+
